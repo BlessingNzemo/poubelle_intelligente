1aaf3e165cec08f9597cc4966f4cf345b0b9072c
Blessing Nzemo <blessingnzemojph@gmail.com>
2025-02-02 21:19:25 +0100

    Ajout de la route gestion dans le middleware d'authentification

--- routes/auth.php
+++ routes/auth.php
@@ -60,4 +60,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/landing', function () {
             return view('landing');
         });
+    Route::get('/gestion', function () {
+        return view('dashboard');
+    });
 });
